972057d590987b080bbf57bc717ffb32ef67a58c
Dennis Perrett <dperrett@lion-linux.is.localnet>
2025-03-31 15:08:29 +0200

    added global orient operations functions to utils.rotation_tools.py

--- src/utils/rotation_tools.py
+++ src/utils/rotation_tools.py
@@ -177,3 +177,53 @@ def aa2euler(axis_angle, convention: str = "xyz"):
 
 def euler2aa(euler_angles, convention: str = "xyz"):
     return torch.Tensor(R.from_euler(convention, euler_angles, degrees=False).as_matrix())
+
+
+def remove_rotation_from_axis(full_pose: torch.Tensor, axis: int, convention: str = 'xyz') -> torch.Tensor:
+    """
+    Removes rotation around a specified axis from the global orient of a full pose represented in axis-angle format.
+
+    Args:
+        full_pose (torch.Tensor): A tensor of shape (batch_size, pose_dim) representing full body pose,
+                                  where the first 3 values correspond to global orientation in axis-angle format.
+        axis (int): The index of the rotation axis to be zeroed (0 for X, 1 for Y, 2 for Z).
+        convention (str, optional): The Euler angle convention used for conversion. Defaults to 'xyz'.
+
+    Returns:
+        torch.Tensor: The modified pose tensor with the specified axis rotation removed.
+    """
+    global_orient = full_pose[:, :3]
+    euler_angles = aa2euler(global_orient) #R.from_matrix(aa2matrot(global_orient)).as_euler(convention, degrees=False)
+    euler_angles[:,axis] = 0 # remove plane rotation
+
+    global_orient = euler2aa(euler_angles) #torch.Tensor(R.from_euler(convention, euler_angles, degrees=False).as_matrix())
+    global_orient = matrot2aa(global_orient)
+    
+    full_pose[:, :3] = global_orient
+    return full_pose
+
+def merge_global_orients_along_axis(additional: torch.Tensor, base: torch.Tensor, axis: int) -> torch.Tensor:
+    """
+    Merges the global orientation of the `additional` pose into `base` along a specified axis.
+
+    Args:
+        additional (torch.Tensor): Tensor of shape (batch_size, pose_dim) containing the additional pose.
+        base (torch.Tensor): Tensor of shape (batch_size, pose_dim) containing the base pose.
+        axis (int): The axis (0 for X, 1 for Y, 2 for Z) along which the global orientation is merged.
+
+    Returns:
+        torch.Tensor: A new tensor with the modified global orientation.
+    """
+    base = base.clone()
+    base_global_orient = base[:, :3] # full_pose -> GO
+    addi_global_orient = additional[:, 3] # full pose -> GO
+
+    base_euler_angles = aa2euler(base_global_orient)
+    addi_euler_angles = aa2euler(addi_global_orient)
+
+    base_euler_angles[:, axis] = addi_euler_angles[:, axis]
+
+    merged_global_orient = euler2aa(base_euler_angles)
+    base[:, :3] = merged_global_orient
+
+    return base
