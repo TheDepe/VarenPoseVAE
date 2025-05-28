d470928f6679532b6ed7a1ad6339a3fe992a296e
Dennis Perrett <dperrett@lion-linux.is.localnet>
2025-05-28 13:42:27 +0200

    small bug fix

--- src/models/varen_poser.py
+++ src/models/varen_poser.py
@@ -225,7 +225,7 @@ class VarenPoser(nn.Module):
         # remove go z rotation
         prepared_pose = remove_rotation_from_axis(full_pose, axis=2)
 
-        regularised_pose = self(prepared_pose)
+        regularised_pose = self(prepared_pose)['body_pose']
 
         # add original z rotation back
         output_pose = merge_global_orients_along_axis(
